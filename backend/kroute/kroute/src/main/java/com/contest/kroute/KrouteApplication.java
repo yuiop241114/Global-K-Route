e3a156b8727b1973f769938518380c42162bc102
kjsHome <was1239@gmail.com>
2026-05-25 01:16:33 +0900

    백엔드 파일 구성

---
+++ backend/kroute/kroute/src/main/java/com/contest/kroute/KrouteApplication.java
@@ -0,0 +1,13 @@
+package com.contest.kroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrouteApplication.class, args);
+	}
+
+}
---
+++ backend/kroute/kroute/src/test/java/com/contest/kroute/KrouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contest.kroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
